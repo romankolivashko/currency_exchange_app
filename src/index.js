1eea3d8e468c18852b303831ffd1eb11fccd0eee
Roman Kolivashko <rkolivashko@gmail.com>
2021-09-10 09:50:57 -0700

    API exchange service tested OK

--- src/index.js
+++ src/index.js
@@ -2,17 +2,39 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeService from './services/rates-service.js';
+
+function clearFields() {
+  $('#base-amount').val("");
+  $('#tgt-amount').val("");
+  $('.showErrors').text("");
+  //$('.showRate').text("");
+}
+
+function getElements(response) {
+  if (response) {
+    $('.showRate').text(`Converting from ${response.base_code} to ${response.target_code} will yield ${response.conversion_result}`);
+    $('#base-amount').val();
+    $('#tgt-amount').val(response.conversion_result);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+async function makeApiCall(baseCurr, tgtCurr, amount) {
+  const response = await ExchangeService.getRate(baseCurr, tgtCurr, amount);
+  console.dir(response);
+  getElements(response);
+  //return amount;
+}
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    //const city = $('#location').val();
-    // $('#location').val("");
-
-
-
-    // function getElements(response) {
-    //   $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-    //   $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    // }
+  $('#getRates').click(function() {
+    let baseCurr = $('#base-curr').val();
+    let tgtCurr = $('#tgt-curr').val();
+    let amount = $('#base-amount').val();
+    //$('#base-amount').val(amount);
+    clearFields();
+    makeApiCall(baseCurr, tgtCurr, amount);
   });
 });
--- src/services/rates-service.js
+++ src/services/rates-service.js
@@ -1,6 +1,18 @@
 export default class ExchangeService {  
   static async getRate(baseCurr, tgtCurr, amount) {
     try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCurr}/${tgtCurr}/${amount}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+
+  static async getRateUSD() {
+    try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/process.env.API_KEY/latest/USD`);
       if (!response.ok) {
         throw Error(response.statusText);
@@ -10,4 +22,6 @@ export default class ExchangeService {
       return error.message;
     }
   }
+
+
 }
