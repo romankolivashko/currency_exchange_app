b3fc341812d780193e26a47062f382abfea9fcb4
Roman Kolivashko <rkolivashko@gmail.com>
2021-09-10 09:40:06 -0700

    Exposed key, hard reset, redo index.js and service

--- src/index.js
+++ src/index.js
@@ -5,25 +5,14 @@ import './css/styles.css';
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
+    //const city = $('#location').val();
+    // $('#location').val("");
 
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
 
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
 
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
+    // function getElements(response) {
+    //   $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+    //   $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    // }
   });
 });
